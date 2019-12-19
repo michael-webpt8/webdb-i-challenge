c7d5ccb12008cf86d2b71dbb215192e093cbce66
Mikey Williams <manicdesigner@gmail.com>
2019-12-19 17:03:48 -0500

    post and put

--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -20,4 +20,32 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
+router.post('/', async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name, // string
+            budget: req.body.budget // num
+        }
+        const [id] = await db('accounts').insert(payload);
+        res.json(await db('accounts').where("id", id).first());
+    }
+    catch (err) {
+        next(err);
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const updatePayload = {
+            name: req.body.name, // string
+            budget: req.body.budget // num
+        }
+        await db('accounts').where('id', req.params.id).update(updatePayload);
+
+        res.status(await db('accounts').where('id', req.params.id).first());
+    } catch (err) {
+        next(err);
+    }
+})
+
 module.exports = router;
