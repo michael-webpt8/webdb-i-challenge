0dd197de4be371c53cc749309d62927316ee87f9
Mikey Williams <manicdesigner@gmail.com>
2019-12-19 16:40:16 -0500

    build out error

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
+const accountsRouter = require('./routes/accountsRoute');
+
 
 const server = express();
-
 server.use(express.json());
+
+server.use('/accounts', accountsRouter);
+
+
+server.use((req, res) => {
+    res.status(404).json({ message: '404 missing route.' })
+})
+
+server.use((err, res, req, next) => {
+    console.log(err);
+    res.status(500).json({ message: 'An error occurred.' })
+})
 
 module.exports = server;
