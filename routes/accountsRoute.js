451bd9f63cd8dbad48495df9c178509bbcb63e65
Michael Williams <manicdesigner@gmail.com>
2019-12-20 21:28:56 -0500

    fixed up code

--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -4,48 +4,71 @@ const db = require('../data/dbConfig');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    try {
-        res.json(await db('accounts').select())
-    }
-    catch (err) {
-        next(err);
-    }
-})
+  try {
+    res.json(await db('accounts').select());
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get('/:id', async (req, res, next) => {
-    try {
-        res.json(await db("accounts").where("id", req.params.id).first())
-    } catch (err) {
-        next(err);
-    }
-})
+  try {
+    res.json(
+      await db('accounts')
+        .where('id', req.params.id)
+        .first()
+    );
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post('/', async (req, res, next) => {
-    try {
-        const payload = {
-            name: req.body.name, // string
-            budget: req.body.budget // num
-        }
-        const [id] = await db('accounts').insert(payload);
-        res.json(await db('accounts').where("id", id).first());
-    }
-    catch (err) {
-        next(err);
-    }
-})
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    };
+    const [id] = await db('accounts').insert(payload);
+    res.json(
+      await db('accounts')
+        .where('id', id)
+        .first()
+    );
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.put('/:id', async (req, res, next) => {
-    try {
-        const updatePayload = {
-            name: req.body.name, // string
-            budget: req.body.budget // num
-        }
-        await db('accounts').where('id', req.params.id).update(updatePayload);
+  try {
+    const updatePayload = {
+      name: req.body.name, // string
+      budget: req.body.budget // num
+    };
+    await db('accounts')
+      .where('id', req.params.id)
+      .update(updatePayload);
 
-        res.status(await db('accounts').where('id', req.params.id).first());
-    } catch (err) {
-        next(err);
-    }
-})
+    res.json(
+      await db('accounts')
+        .where('id', req.params.id)
+        .first()
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await db('accounts')
+      .where({ id: req.params.id })
+      .del();
+    res.json(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const express = require('express');
 const db = require('./data/dbConfig.js');
 const accountsRouter = require('./routes/accountsRoute');
 
-
 const server = express();
 server.use(express.json());
 
 server.use('/accounts', accountsRouter);
 
-
 server.use((req, res) => {
-    res.status(404).json({ message: '404 missing route.' })
-})
+  res.status(404).json({ message: '404 missing route.' });
+});
 
-server.use((err, res, req, next) => {
-    console.log(err);
-    res.status(500).json({ message: 'An error occurred.' })
-})
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: 'An error occurred.' });
+});
 
 module.exports = server;
