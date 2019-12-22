6250da4b685e00c7f1e1c0449202eb3c95e1296b
Michael Williams <manicdesigner@gmail.com>
2019-12-21 20:10:33 -0500

    stretch queries

--- queries.sql
+++ queries.sql
@@ -30,4 +30,8 @@ UPDATE Customers
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+SELECT DISTINCT COUNT(City) as 'City Count' FROM Customers;
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
+SELECT * FROM Suppliers WHERE length(SupplierName) > 20;
